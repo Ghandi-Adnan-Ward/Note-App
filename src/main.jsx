b44f807ab5d082b7fe379de27564cb4808f28d8f
Ghandi Adnan Ward <165215062+Ghandi-Adnan-Ward@users.noreply.github.com>
2025-10-02 09:44:02 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router,   } from "react-router-dom"
-
+import { HashRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    < Router basename='/Note-App'>
+    < HashRouter>
         <App />
-    </Router>
+    </HashRouter>
   </StrictMode>,
 )
